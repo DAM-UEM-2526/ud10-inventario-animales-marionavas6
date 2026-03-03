4abfeaa342d15feb6d867470e56deb8bc9ca9bd5
Mario <navasmario481@gmail.com>
2026-03-03 16:14:59 +0100

    1º commit

---
+++ src/EJ1/Mascotas.java
@@ -0,0 +1,10 @@
+package EJ1;
+
+public class Mascotas {
+	// Primero pongo los atributos
+	private String nombre;
+	private int edad;
+	private String estado;
+	private String fechaNacimiento;
+	// y ahora hago el constructor
+}
